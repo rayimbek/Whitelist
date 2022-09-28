97db5782f6353db33192c3814a78a4068e3f9290
Raiymbek <rayimbek144@gmail.com>
2022-09-28 16:46:48 +0600

    New project

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+ // Whitelist contract address 0xfd49cEee22870ff5796713Eeb49B60B603BFaCC5
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
